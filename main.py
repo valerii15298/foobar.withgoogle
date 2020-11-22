7c79abb46ec63e023c9f147b8e89be72f465d125
Valerii <valerii15298@gmail.com>
2020-11-22 16:45:24 +0200

    request first challenge

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def SieveOfEratosthenes(n):
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+
+            # Update all multiples of p
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    # Print all prime numbers
+    for p in range(n + 1):
+        if prime[p]:
+            print p
 
 
-print(2)
+def solution(i):
+    pass
+
+
+solution(100)
