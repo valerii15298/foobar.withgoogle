1b382a62f1330b00e024a9cd795fd69610028881
Valerii <valerii15298@gmail.com>
2020-11-22 17:36:29 +0200

    requet1 submited

--- request1/solution.py
+++ request1/solution.py
@@ -1,2 +1,16 @@
-def solution(i):
-    pass
+def solution(index):
+    # Your code here
+    n = 20231
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    string = ''
+    # Concat all prime numbers
+    for p in range(2, n + 1):
+        if prime[p]:
+            string += str(p)
+    return string[index:index + 5]
