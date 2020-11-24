bd2e094fbedf11bf07af7693c4e2b2250af1b2fa
Valerii <valerii15298@gmail.com>
2020-11-24 17:05:39 +0200

    dot on line with max distance

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import math
+from fractions import Fraction as f
 
 
 def solution(dimensions, my_position, guard_position, distance):
+    distance = distance ** 2
     x_room, y_room = dimensions
 
     x_count_mirrors = int(math.ceil(float(distance) / x_room))
@@ -43,19 +45,79 @@ def solution(dimensions, my_position, guard_position, distance):
     guard_mirrors = get_mirrors(guard_position)
 
     def get_distance(pos1, pos2):
-        return 1
+        return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
+
+    def is_between(b, c):
+        distance = 25
+
+        crossproduct = c[1] * b[0] - c[0] * b[1]
+        if abs(crossproduct) != 0:
+            return False
+
+        hypo = b[0] ** 2 + b[1] ** 2
+
+        if hypo < distance:
+            k = f(b[0], b[1]) ** 2
+            bx2 = f(distance, (1 / k) + 1)
+            by2 = f(distance, k + 1)
+
+            bx = math.sqrt(bx2) * (1 if b[0] >= 0 else -1)
+            by = math.sqrt(by2) * (1 if b[1] >= 0 else -1)
+
+            hypo = bx2 + by2
+            assert hypo == distance
+
+            if (c[0] * bx + c[1] * by) < 0:
+                return False
+
+            c = list(c)
+            c[0] = c[0] ** 2
+            c[1] = c[1] ** 2
+
+            l = ((c[0] * bx2) + (c[1] * by2) - (hypo ** 2)) ** 2
+            r = 4 * c[0] * c[1] * bx2 * by2
+
+            if l < r:
+                return False
+
+        else:
+            dot_product = c[0] * b[0] + c[1] * b[1]
+            if dot_product < 0:
+                return False
+
+            if dot_product > hypo:
+                return False
+
+        return True
+
+    r = is_between((1, 1), (2, 2))
+    print r
+    return 0
 
     def check(pos1, pos2):
         if get_distance(pos1, pos2) > distance:
             return False
-
+        for my_pos in my_mirrors:
+            if is_between(pos1, pos2, my_pos):
+                return False
+        return True
 
     result_count = 0
     for guard_pos in guard_mirrors:
         if check(my_position, guard_position):
             result_count += 1
 
-    return 1
+    return result_count
 
 
-solution([5, 3], [1, 1], [2, 1], 6)
+res = solution([3, 2], [1, 1], [2, 1], 4)
+# print res
+
+# x1, y1 = (3.0, 4.0)
+# x2, y2 = (6.0, 8.0)
+# d = 15.0
+#
+#
+# y3 = d ** 2 / (((y2 - y1) / (x2 - x1)) ** 2 + 1)
+#
+# print y3
