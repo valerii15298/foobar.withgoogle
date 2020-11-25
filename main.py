bd1afdcd4a1f2bc672f818894fd1c8a938183065
Valerii <valerii15298@gmail.com>
2020-11-25 21:54:30 +0200

    ll

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import itertools
 
+
 def solution(w, h, s):
     count = w * h
+
     for i in range(count):
-        possible_reveals =
+        # possible_reveals =
         pass
     return 0
+
+
+bb = 20 ** 144
+i = 0
+while i < bb:
+    i += 1
